0e32885b3e5618fb43fff5f68b7592ea761b631c
Hayden Walls <hwilliams.deva@gmail.com>
2018-11-20 15:18:22 -0800

    add styles etc

--- whosgay.js
+++ whosgay.js
@@ -1,6 +1,9 @@
 $(document).ready(() => {
   $('.result').hide();
+  $('.images').hide();
   $('button').on('click', () => {
     $('.result').slideDown(600);
+    $('.images').fadeIn(300);
+    $('.form').hide();
   });
 });
